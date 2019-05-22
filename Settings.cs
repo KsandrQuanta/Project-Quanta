d3a57153268b4887a687faf02aa821344eb53fdc
Mary1485 <50766339+Mary1485@users.noreply.github.com>
2019-05-22 17:08:53 +0300

    Add files via upload

---
+++ Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+
+public class Settings : MonoBehaviour {
+    public AudioMixer am;
+    public bool isFullScreen;
+    public void AudioVolume(float sliderValue){
+        am.SetFloat("masterVolume", sliderValue);
+    }
+    public void FullScreenToggle() {
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
+    
+		
+	
+}
