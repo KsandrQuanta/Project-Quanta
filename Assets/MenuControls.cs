285380bdcaea1225f2e3e6f0baf55cb69721e3b5
Mary1485 <50766339+Mary1485@users.noreply.github.com>
2019-05-25 20:15:54 +0300

    Add files via upload

---
+++ Assets/MenuControls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour {
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("game");
+    }
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+    public void SettingsPressed()
+    {
+        SceneManager.LoadScene("sett");
+    }
+}
---
+++ Assets/setting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class setting : MonoBehaviour {
+
+    public void BackPressed()
+    {
+        SceneManager.LoadScene("menu");
+    }
+    public bool isFullScreen;
+    
+    public void FullScreenToggle()
+    {
+        if (isFullScreen = !isFullScreen)
+            Screen.fullScreen = isFullScreen;
+        else isFullScreen = !Screen.fullScreen;
+    }
+}
